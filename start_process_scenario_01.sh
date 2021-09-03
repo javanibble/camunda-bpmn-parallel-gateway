ded37bd9538d97f33a322a8122356d9c392bff4d
andre-mare <andre.mare@gmail.com>
2021-09-03 16:10:32 +0200

    Update Rest Endpoint

--- start_process_scenario_01.sh
+++ start_process_scenario_01.sh
@@ -1,1 +1,1 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json'
+curl --location --request POST 'http://localhost:8080/engine-rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json'
